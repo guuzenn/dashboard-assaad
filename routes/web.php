5b1966a00db2431dbd09c419b90eb1bd77b53356
Hotniels <diasi130706@gmail.com>
2025-05-01 12:06:02 +0700

    Menyimpan perubahan

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\KelasController;
 use App\Http\Controllers\MataPelajaranController;
 use App\Http\Controllers\NilaiController;
 use App\Http\Controllers\PivotMataPelajaranKelas;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::view('/login', 'auth.login')->name('login');
+// Routing Auth (Login)
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 
 // Test
 
